3a78618382b7c1e6ab3bdeda38a8607d924cad33
Chao Xu <xuchao@google.com>
2015-04-29 11:17:55 -0700

    first

--- pkg/api/latest/latest.go
+++ pkg/api/latest/latest.go
@@ -103,6 +103,7 @@ func init() {
 		},
 	)
 	// list of versions we support on the server
+	// versions should be listed in the order of perferred versions first
 	versions := []string{"v1beta1", "v1beta2", "v1beta3"}
 
 	// versions that used mixed case URL formats
