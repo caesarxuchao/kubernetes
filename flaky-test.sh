8bff4cce824da57227c9abc7c39ee435bc063d21
Chao Xu <xuchao@google.com>
2015-06-16 15:30:35 -0700

    improve

--- flaky-test.sh
+++ flaky-test.sh
@@ -3,9 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-for i in `seq 1 20`
+for i in `seq 1 100`
 do
-echo -e "/n/n/nCHAO==================================================/n/n/n"
+echo -e "\n\n\nCHAO==================================================\n\n\n"
 echo -e "CHAO: i=$i"
 KUBE_TEST_API_VERSIONS=v1 KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4 LOG_LEVEL=4 ./hack/test-integration.sh
 KUBE_TEST_API_VERSIONS=v1beta3 KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4 LOG_LEVEL=4 ./hack/test-integration.sh
