029c9f0b3208149241e687bba2320aa25c0e75fa
Chao Xu <xuchao@google.com>
2016-10-17 17:02:47 -0700

    convert.sh

--- pkg/controller/convert.sh
+++ pkg/controller/convert.sh
@@ -23,6 +23,8 @@ $K1/grep-sed.sh "k8s.io/client-go/1.5/kubernetes/typed/autoscaling/unversioned" 
 # PART II: convert type imports
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/api\"" "k8s.io/client-go/1.5/pkg/api\"\n\"k8s.io/client-go/1.5/pkg/api/v1\""
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/api" "k8s.io/client-go/1.5/pkg/api"
+# storage should be v1beta1
+$K1/grep-sed.sh "\"k8s.io/client-go/1.5/pkg/apis/storage" "storage \"k8s.io/client-go/1.5/pkg/apis/storage/v1beta1"
 # don't convert "k8s.io/kubernetes/pkg/api/validation"
 $K1/grep-sed.sh "k8s.io/client-go/1.5/pkg/api/validation" "k8s.io/kubernetes/pkg/api/validation" 
 # don't convert "k8s.io/kubernetes/pkg/api/annotations"
@@ -39,6 +41,14 @@ $K1/grep-sed.sh "k8s.io/kubernetes/pkg/apis" "k8s.io/client-go/1.5/pkg/apis"
 gofmt -w -r 'api.a -> v1.a' ./
 gofmt -w -r 'v1.Scheme -> api.Scheme' ./
 gofmt -w -r 'v1.Unversioned -> api.Unversioned' ./
+gofmt -w -r 'v1.StrategicMergePatchType -> api.StrategicMergePatchType' ./
+gofmt -w -r 'v1.ObjectNameField -> api.ObjectNameField' ./
+gofmt -w -r 'v1.SecretTypeField -> api.SecretTypeField' ./
+gofmt -w -r 'v1.PodHostField -> api.PodHostField' ./
+gofmt -w -r 'v1.SecretTypeField -> api.SecretTypeField' ./
+gofmt -w -r 'v1.Codecs -> api.Codecs' ./
+git grep -l "v1.Resource(" | xargs sed -i "s,v1.Resource(,api.Resource(,g"
+gofmt -w -r 'v1.PatchType -> api.PatchType' ./
 
 # Don't rewrite metrics_api to metrics_v1
 $K1/grep-sed.sh "metrics_v1" "metrics_api"
@@ -49,7 +59,18 @@ $K1/grep-sed.sh "k8s.io/kubernetes/pkg/watch" "k8s.io/client-go/1.5/pkg/watch"
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/runtime" "k8s.io/client-go/1.5/pkg/runtime"
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/labels" "k8s.io/client-go/1.5/pkg/labels"
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/fields" "k8s.io/client-go/1.5/pkg/fields"
-$K1/grep-sed.sh "k8s.io/kubernetes/pkg/types" "k8s.io/client-go/1.5/pkg/types"
+# SPECIAL:types.NodeName is used in pkg/cloudprovider as well, so we want to keep using main repo's types.NodeName
+{ git grep -l "\<types.UID" || true ; } | while read -r target ; do
+    if [[ "${target}" == *"convert.sh"* ]]; then
+        continue
+    fi
+    if grep -q "\<types.NodeName" "${target}"; then
+        sed -i 's,"k8s.io/kubernetes/pkg/types",k8stypes "k8s.io/kubernetes/pkg/types"\n   "k8s.io/client-go/1.5/pkg/types",g' "${target}"
+        sed -i 's,types.NodeName,k8stypes.NodeName,g' "${target}"
+    else
+        sed -i 's,k8s.io/kubernetes/pkg/types,k8s.io/client-go/1.5/pkg/types,g' "${target}"
+    fi
+done
 
 #PART V: use volumeshadow
 $K1/grep-sed.sh "k8s.io/kubernetes/pkg/volume" "k8s.io/kubernetes/pkg/volumeshadow"
@@ -59,7 +80,7 @@ set +x
 
 #PART VI: rewrite labelselectors, to call .String()
 git grep -l "\<LabelSelector.*}" | xargs sed -i "/unversioned.LabelSelector/b; s/\<LabelSelector\(.*\)}/LabelSelector\1.String()}/g"
-git grep -l "FieldSelector =" | xargs sed -i "s/FieldSelector =\(.*\)$/FieldSelector =\1.String()/g".String()
+git grep -l "FieldSelector =" | grep -v convert.sh | xargs sed -i "s/FieldSelector =\(.*\)$/FieldSelector =\1.String()/g"
 
 #pkg/watch
 #pkg/runtime
