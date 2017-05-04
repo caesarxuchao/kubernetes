ae413b6756fd535b969606aaa561a317e7305c58
Chao Xu <xuchao@google.com>
2017-05-04 15:41:49 -0700

    no need to cherry-pick, fixed in head

--- staging/src/k8s.io/kube-aggregator/pkg/apis/apiregistration/v1alpha1/register.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apis/apiregistration/v1alpha1/register.go
@@ -27,6 +27,11 @@ const GroupName = "apiregistration.k8s.io"
 // SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
 
+// Resource takes an unqualified resource and returns back a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
 var (
 	// TODO: move SchemeBuilder with zz_generated.deepcopy.go to k8s.io/api.
 	// localSchemeBuilder and AddToScheme will stay in k8s.io/kubernetes.
